a1fb66f32283a4d450c3212380301d125d2346bf
songmilee <lsm941010@gmail.com>
2022-08-02 12:02:17 +0900

    [FEAT] 기록 보기 UI 추가

---
+++ app/src/main/java/com/cocaine/myply/feature/ui/keep/KeepDetailFragment.kt
@@ -0,0 +1,11 @@
+package com.cocaine.myply.feature.ui.keep
+
+import com.cocaine.myply.R
+import com.cocaine.myply.core.base.BaseFragment
+import com.cocaine.myply.databinding.FragmentKeepDetailBinding
+
+class KeepDetailFragment: BaseFragment<FragmentKeepDetailBinding>(R.layout.fragment_keep_detail) {
+    override fun setup() {
+
+    }
+}
