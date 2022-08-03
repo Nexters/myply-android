1e1db4e3bee2099487a8876a2884135f934c1942
dddooo9 <dlaehdus99@naver.com>
2022-08-03 23:39:23 +0900

    [ADD] MypageEditFragment UI 추가

---
+++ app/src/main/java/com/cocaine/myply/feature/ui/mypage/MyPageEditFragment.kt
@@ -0,0 +1,10 @@
+package com.cocaine.myply.feature.ui.mypage
+
+import com.cocaine.myply.R
+import com.cocaine.myply.core.base.BaseFragment
+import com.cocaine.myply.databinding.FragmentMypageEditBinding
+
+class MyPageEditFragment : BaseFragment<FragmentMypageEditBinding>(R.layout.fragment_mypage_edit) {
+    override fun setup() {
+    }
+}
