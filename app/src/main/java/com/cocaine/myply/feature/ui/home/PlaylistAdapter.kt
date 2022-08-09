1f64df73d74b640baced6581481b1f973ad3daee
dddooo9 <dlaehdus99@naver.com>
2022-08-09 22:15:43 +0900

    [FEAT] PlaylistAdapter에 ListAdapter 적용

--- app/src/main/java/com/cocaine/myply/feature/ui/home/PlaylistAdapter.kt
+++ app/src/main/java/com/cocaine/myply/feature/ui/home/PlaylistAdapter.kt
@@ -3,13 +3,29 @@ package com.cocaine.myply.feature.ui.home
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.cocaine.myply.R
 import com.cocaine.myply.databinding.ItemPlaylistBinding
 import com.cocaine.myply.feature.data.model.VideoResponse
 
-class PlaylistAdapter : RecyclerView.Adapter<PlaylistAdapter.PlaylistViewHolder>() {
-    val list = ArrayList<VideoResponse>()
+class PlaylistAdapter : ListAdapter<VideoResponse, PlaylistAdapter.PlaylistViewHolder>(diffUtil) {
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<VideoResponse>() {
+            override fun areItemsTheSame(oldItem: VideoResponse, newItem: VideoResponse): Boolean {
+                return oldItem.youtubeVideoId == newItem.youtubeVideoId
+            }
+
+            override fun areContentsTheSame(
+                oldItem: VideoResponse,
+                newItem: VideoResponse
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
 
     class PlaylistViewHolder(private val binding: ItemPlaylistBinding) :
         RecyclerView.ViewHolder(binding.root) {
@@ -33,8 +49,6 @@ class PlaylistAdapter : RecyclerView.Adapter<PlaylistAdapter.PlaylistViewHolder>
     }
 
     override fun onBindViewHolder(holder: PlaylistViewHolder, position: Int) {
-        holder.bind(list[position])
+        holder.bind(currentList[position])
     }
-
-    override fun getItemCount(): Int = list.size
 }
