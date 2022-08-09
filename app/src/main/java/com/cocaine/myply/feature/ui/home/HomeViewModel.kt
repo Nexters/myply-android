5065a9477b3f0f4e22300f7cda5d32e66a91df9a
dddooo9 <dlaehdus99@naver.com>
2022-08-09 22:16:03 +0900

    [FEAT] HomeViewModel 구현

---
+++ app/src/main/java/com/cocaine/myply/feature/ui/home/HomeViewModel.kt
@@ -0,0 +1,23 @@
+package com.cocaine.myply.feature.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.cocaine.myply.core.base.BaseViewModel
+import com.cocaine.myply.feature.data.model.PlaylistOrder
+import com.cocaine.myply.feature.data.model.VideoResponse
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(): BaseViewModel() {
+
+    private val _playlistOrder = MutableLiveData<PlaylistOrder>()
+    val playlistOrder: LiveData<PlaylistOrder> = _playlistOrder
+
+    private val _playlists = MutableLiveData<List<VideoResponse>>()
+    val playlists: LiveData<List<VideoResponse>> = _playlists
+
+    fun updatePlaylistOrder(playlistOrder: PlaylistOrder) {
+        _playlistOrder.value = playlistOrder
+    }
+}
