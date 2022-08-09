6066437d6784cc14b007b8bb3f6509c856235230
dddooo9 <dlaehdus99@naver.com>
2022-08-09 23:25:51 +0900

    [FEAT] MyPageEditFragment의 ViewModel 로직 구현

--- app/src/main/java/com/cocaine/myply/feature/ui/mypage/MyPageEditFragment.kt
+++ app/src/main/java/com/cocaine/myply/feature/ui/mypage/MyPageEditFragment.kt
@@ -1,10 +1,44 @@
 package com.cocaine.myply.feature.ui.mypage
 
+import androidx.fragment.app.activityViewModels
 import com.cocaine.myply.R
 import com.cocaine.myply.core.base.BaseFragment
 import com.cocaine.myply.databinding.FragmentMypageEditBinding
 
 class MyPageEditFragment : BaseFragment<FragmentMypageEditBinding>(R.layout.fragment_mypage_edit) {
+
+    private val myPageViewModel: MyPageViewModel by activityViewModels()
+
+    private lateinit var myPageEditKeywordAdapter: MyPageEditKeywordAdapter
+
+    private lateinit var myPageKeywordAdapter: MyPageKeywordAdapter
+
     override fun setup() {
+        setKeywordAdapter()
+        setKeywordUpdateConfirmClickListener()
+        setViewModel()
+    }
+
+    private fun setKeywordAdapter() {
+        myPageEditKeywordAdapter = MyPageEditKeywordAdapter()
+        binding?.mypageEditList?.adapter = myPageEditKeywordAdapter
+
+        myPageKeywordAdapter = MyPageKeywordAdapter()
+        binding?.mypageEditCheckedList?.adapter = myPageKeywordAdapter
+    }
+
+    private fun setKeywordUpdateConfirmClickListener() {
+        binding?.mypageEditBtn?.setOnClickListener {
+            myPageViewModel.updateKeywords()
+        }
+    }
+
+    private fun setViewModel() {
+        myPageViewModel.baseKeywords.observe(viewLifecycleOwner) {
+            myPageEditKeywordAdapter.submitList(it)
+        }
+        myPageViewModel.checkedKeywords.observe(viewLifecycleOwner) {
+            myPageKeywordAdapter.submitList(it)
+        }
     }
 }
--- app/src/main/java/com/cocaine/myply/feature/ui/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/cocaine/myply/feature/ui/mypage/MyPageViewModel.kt
@@ -12,6 +12,19 @@ class MyPageViewModel @Inject constructor(): BaseViewModel() {
     private val _nickname = MutableLiveData<String>()
     val nickname: LiveData<String> = _nickname
 
+    // 서버에 반영된 선택된 키워드
     private val _keywords = MutableLiveData<List<String>>()
     val keywords: LiveData<List<String>> = _keywords
+
+    // 선택가능한 전체 키워드 목록
+    private val _baseKeywords = MutableLiveData<List<String>>()
+    val baseKeywords: LiveData<List<String>> = _baseKeywords
+
+    // 사용자가 편집하면서 선택한 키워드
+    private val _checkedKeywords = MutableLiveData<List<String>>()
+    val checkedKeywords: LiveData<List<String>> = _checkedKeywords
+
+    fun updateKeywords() {
+        // TODO 새로운 키워드 목록을 서버에 반영
+    }
 }
