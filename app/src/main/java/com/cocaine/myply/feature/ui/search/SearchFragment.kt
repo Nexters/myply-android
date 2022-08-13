5cde819f44e6c41d3803418da29e1ecf79dc6d0d
songmilee <lsm941010@gmail.com>
2022-08-13 15:15:12 +0900

    [FEAT] 검색어 인풋 x 버튼 추가

--- app/src/main/java/com/cocaine/myply/feature/ui/search/SearchFragment.kt
+++ app/src/main/java/com/cocaine/myply/feature/ui/search/SearchFragment.kt
@@ -1,15 +1,15 @@
 package com.cocaine.myply.feature.ui.search
 
 import android.view.View
-import android.view.ViewTreeObserver
+import androidx.core.widget.addTextChangedListener
 import com.cocaine.myply.R
 import com.cocaine.myply.core.base.BaseFragment
 import com.cocaine.myply.databinding.FragmentSearchBinding
 
-class SearchFragment: BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
     override fun setup() {
-        binding?.searchEditText?.setOnFocusChangeListener { view, isFocus ->
-            if(isFocus) {
+        binding?.searchEditTxt?.setOnFocusChangeListener { view, isFocus ->
+            if (isFocus) {
                 binding?.searchBtn?.visibility = View.VISIBLE
             } else {
                 binding?.searchBtn?.visibility = View.INVISIBLE
