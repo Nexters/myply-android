36ef0544a6a8fa01389519553d5a9f5d86706697
songmilee <lsm941010@gmail.com>
2022-08-18 08:34:38 +0900

    [FEAT] 검색 시, 필요 데이터 추가 정의

--- app/src/main/java/com/cocaine/myply/feature/data/model/SearchResponse.kt
+++ app/src/main/java/com/cocaine/myply/feature/data/model/SearchResponse.kt
@@ -1,3 +1,9 @@
 package com.cocaine.myply.feature.data.model
 
-data class SearchResponse(val code: Int, val message: String?, val data: List<VideoResponse>)
+data class SearchResponse(val code: Int, val message: String?, val data: SearchMusicData?)
+
+data class SearchMusicData(val musics: List<VideoResponse>, val nextPageToken: String?)
+
+data class SearchTagResponse(val code: Int, val message: String?, val data: Tags)
+
+data class Tags(val tags: List<String>)
--- app/src/main/java/com/cocaine/myply/feature/data/model/VideoResponse.kt
+++ app/src/main/java/com/cocaine/myply/feature/data/model/VideoResponse.kt
@@ -1,10 +1,11 @@
 package com.cocaine.myply.feature.data.model
 
 data class VideoResponse(
-    val youtubeVideoId: String,
+    val youtubeVideoId: String = "-1", // TODO: 통신 시, nullable 값으로 인한 파싱 에러가 발생함
     val thumbnailURL: String,
-    val youtubeTags: List<String>,
+    val youtubeTags: List<String>?,
     val title: String,
     val videoDeepLink: String,
-    val isLiked: Boolean
+    val isLiked: Boolean = false,
+    val isMemoed: Boolean = false
 )
