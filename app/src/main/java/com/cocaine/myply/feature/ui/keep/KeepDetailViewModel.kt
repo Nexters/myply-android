cc1705be8d1ccbc783775b238a2c61c023af3c9d
songmilee <lsm941010@gmail.com>
2022-08-23 19:01:24 +0900

    [ADD] MemoFragment에서 넘겨 받은 메모 정보 MemoDetail 상에 표시

--- app/src/main/java/com/cocaine/myply/feature/ui/keep/KeepDetailViewModel.kt
+++ app/src/main/java/com/cocaine/myply/feature/ui/keep/KeepDetailViewModel.kt
@@ -2,14 +2,38 @@ package com.cocaine.myply.feature.ui.keep
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.cocaine.myply.core.base.BaseViewModel
+import com.cocaine.myply.feature.data.model.MemoResponse
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class KeepDetailViewModel @Inject constructor(): BaseViewModel() {
+@HiltViewModel
+class KeepDetailViewModel @Inject constructor(private val usecase: KeepDetailUsecase): BaseViewModel() {
     private val _isWrited = MutableLiveData<Boolean>(false)
     val isWrited: LiveData<Boolean> = _isWrited
 
+    private val _memoDetail = MutableLiveData<MemoResponse>()
+    val memoDetail: LiveData<MemoResponse> = _memoDetail
+
     fun updateIsWritable(writeCount: Int) {
         _isWrited.value = writeCount > 0
+    }
+
+    fun updateMemoData(memoResponse: MemoResponse) {
+        _memoDetail.value = memoResponse
+    }
+
+    fun getMemo(memoId: String) {
+        viewModelScope.launch(Dispatchers.IO + coroutineExceptionHandler) {
+            val result = usecase.getMemo(memoId)
+
+            withContext(Dispatchers.Main) {
+                _memoDetail.value = result
+            }
+        }
     }
 }
