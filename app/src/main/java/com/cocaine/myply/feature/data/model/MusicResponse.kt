6f48c6edf5329d45e1bd4c691659bdbe90b48bd1
dddooo9 <dlaehdus99@naver.com>
2022-08-24 17:40:18 +0900

    [FEAT] Music API Response를 받기 위한 data class 추가 정의

--- app/src/main/java/com/cocaine/myply/feature/data/model/MusicResponse.kt
+++ app/src/main/java/com/cocaine/myply/feature/data/model/MusicResponse.kt
@@ -4,6 +4,10 @@ enum class MemoState {
     EMPTY, LIKED, FILLED
 }
 
+data class MusicResponse(val code: Int, val message: String?, val data: MusicDataWithNextPageToken?)
+
+data class MusicDataWithNextPageToken(val musics: List<MusicData>, val nextPageToken: String?)
+
 data class MusicData(
     val youtubeVideoId: String = "-1", // TODO: 통신 시, nullable 값으로 인한 파싱 에러가 발생함
     val thumbnailURL: String,
